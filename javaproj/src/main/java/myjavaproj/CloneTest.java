bd1a8ec997b95fc6d9519a0efe67f79de78be6b0
Achyuth <kondasaniachyuth@gmail.com>
2023-11-27 12:49:58 +0530

    Committed 2 files to branch2

--- javaproj/src/main/java/myjavaproj/CloneTest.java
+++ javaproj/src/main/java/myjavaproj/CloneTest.java
@@ -16,15 +16,26 @@ class CloneTest implements Cloneable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		a++;
+		a--;
+		++a;
+		--a;
 		try {
 			CloneTest t1 = new CloneTest("Achyuth", 100);
 			CloneTest t2 = (CloneTest) t1.clone();
 			System.out.println(t1.rollno + " " + t1.name);
 			System.out.println(t2.rollno + " " + t2.name);
-		} catch (CloneNotSupportedException c) {
-
+		} catch (CloneNotSupportedException c) {}
+		int i=225;
+		int sum = 0;
+		while(i>0) {
+			sum = sum + i%10;
+			int r=i%10;
+			i=i/10;
 		}
-
+		System.out.println(sum);
+		
 	}
 
 }
